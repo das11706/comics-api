9d74b7048cd8f44d0de7113cce531c269a1099a4
Daniel Suarez <suarez.daniel101@gmail.com>
2023-07-22 16:42:58 -0400

    refactor renderComics function

--- comics_client/src/index.js
+++ comics_client/src/index.js
@@ -1,3 +1,105 @@
+// document.addEventListener("DOMContentLoaded", () => {
+ 
+//   const reviewForm = document.getElementById("create-review-form");
+//   // const newReviewDescription = document.getElementById("new-review-description");
+//   // const newReviewList = document.getElementById("reviews");
+//   reviewForm.addEventListener("submit", createReview);
+
+//   // submitData();
+//   fetchComics();
+// });
+
+
+// const appendNewReview = (review) => {
+//   document.getElementById("reviews").appendChild(review);
+// };
+
+
+// const createReview = (event) => {
+//   event.preventDefault();
+//   const newReviewDescription = document.getElementById("new-review-description");
+//   const newReview = document.createElement("li");
+//   newReview.innerText = newReviewDescription.value;
+  
+//   const button = document.createElement("button");
+//   button.innerText = "X";
+
+
+//   button.addEventListener("click", deleteReview);
+  
+//   newReview.appendChild(button);
+
+//   appendNewReview(newReview);
+//   event.target.reset();
+// }
+
+//   const deleteReview = () => {
+//   // event.preventDefault();
+//   // document.getElementById("reviews").removeChild();
+//   let reviews = document.getElementById("reviews");
+//   reviews.removeChild(reviews.firstElementChild);
+
+//   // if (reviews.hasChildNodes()){
+//   //   reviews.removeChild(reviews.children[0]);
+//   // }
+
+// }
+
+
+// // function deleteReview() {
+// //   event.preventDefault();
+// //   document.getElementById("reviews").removeChild();
+// // }
+
+// function submitData(id, description, comic, reader) {
+//   return fetch("http://localhost:3000/reviews", {
+//     method : "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//       newReviewDescription,
+//     })
+//   })
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(json => renderReview(json))
+//     // .then(function(object) {
+//     //   document.body.innerHTML = object["id"]
+//     // }) 
+//     // .catch(function(error) {
+    
+//     //   document.body.innerHTML = error.message
+//     // })
+// }
+
+// function fetchComics() {
+//   return fetch("http://localhost:3000/reviews")
+//   .then(resp => resp.json())
+//   .then(json => renderComics(json))
+// }
+
+
+// function renderComics(reviews) {
+//   const main = document.querySelector('main');
+//   reviews.forEach(review => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = review.comic.title;
+//     main.appendChild(h2);
+
+//     const h3 = document.createElement('h3');
+//     h3.innerHTML = review.comic.artist;
+//     main.appendChild(h3);
+//   });
+// }
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
  
   const reviewForm = document.getElementById("create-review-form");
@@ -5,8 +107,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // const newReviewList = document.getElementById("reviews");
   reviewForm.addEventListener("submit", createReview);
 
-  // submitData();
-  fetchReviews();
+  submitData();
+  fetchComics();
 });
 
 
@@ -38,54 +140,48 @@ const createReview = (event) => {
   // document.getElementById("reviews").removeChild();
   let reviews = document.getElementById("reviews");
   reviews.removeChild(reviews.firstElementChild);
-
-  // if (reviews.hasChildNodes()){
-  //   reviews.removeChild(reviews.children[0]);
-  // }
-
 }
-
 
 // function deleteReview() {
 //   event.preventDefault();
 //   document.getElementById("reviews").removeChild();
 // }
 
-// function submitData(id, description, comic, reader) {
-//   return fetch("http://localhost:3000/reviews", {
-//     method : "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify({
-//       id,
-//       description,
-//       comic,
-//       reader,
-//     })
-//   })
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(json => renderComics(json))
-//     // .then(function(object) {
-//     //   document.body.innerHTML = object["id"]
-//     // }) 
-//     // .catch(function(error) {
+function submitData(id, description, comic, reader) {
+  return fetch("http://localhost:3000/reviews", {
+    method : "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      id,
+      description,
+      comic,
+      reader
+    })
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(json => createReview(json))
+    // .then(function(object) {
+    //   document.body.innerHTML = object["id"]
+    // }) 
+    // .catch(function(error) {
     
-//     //   document.body.innerHTML = error.message
-//     // })
-// }
+    //   document.body.innerHTML = error.message
+    // })
+}
 
-function fetchReviews() {
+function fetchComics() {
   return fetch("http://localhost:3000/reviews")
   .then(resp => resp.json())
-  .then(json => renderReviews(json))
+  .then(json => renderComics(json))
 }
 
 
-function renderReviews(reviews) {
+function renderComics(reviews) {
   const main = document.querySelector('main');
   reviews.forEach(review => {
     const h2 = document.createElement('h2');
