d53ee8e0842406fc1102c7ec5e15ebe16adfc831
Daniel Suarez <suarez.daniel101@gmail.com>
2023-07-31 17:54:57 -0400

    Include put route for comics and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
+  # resources :reviews
   get '/reviews' => 'reviews#index'
   get '/reviews/:id' => 'reviews#show'
   post '/reviews' => 'reviews#create'
   patch 'reviews/:id' => 'reviews#update'
+  put '/reviews/:id' => 'reviews#update'
   delete '/reviews/:id' => 'reviews#destroy'
 
   resources :readers
@@ -14,6 +15,7 @@ Rails.application.routes.draw do
   # get '/comics' => 'comics#new'
   post '/comics' => 'comics#create'
   patch '/comics/:id' => 'comics#update'
+  put '/comics/:id' => 'comics#update'
   delete '/comics/:id' => 'comics#destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
